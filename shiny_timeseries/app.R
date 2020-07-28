3e2cd5e7e439aeb5bc4175d190e28723e0c8aba6
Julia Blanchard <juliab6@DOM-IMA-6G5JHD2.local>
2020-07-29 01:49:22 +1000

    shiny timeseries

--- shiny_timeseries/app.R
+++ shiny_timeseries/app.R
@@ -16,23 +16,18 @@ ui <- fluidPage(
    # Application title
    titlePanel("Exploring Time Series"),
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        sliderInput("r_max", "Rmax scalar:", min = 0.01, max = 5, value = 1,
-                    step = 0.1),
-     
-        sliderInput("eRepro", "Reproductive Efficiency:", min = 0.0001, max = 1, value = 1,
-                    step = 0.01),
-        sliderInput("kappa", "Plankton Multiplier:", min = 0.1, max = 10, value = 1,
+     fluidRow(
+       column(4, wellPanel(
+         sliderInput("Rmax", "log10 Maximum Recruitment:", min = round(log10(min(params@species_params$R_max)),0.1), max = round(log10(max(params@species_params$R_max)),0.1), value = 14,
+                       step = 0.1),
+         sliderInput("erepro", "log10 Reproductive Efficiency:", min = -8, max = 1, value = -2,
                      step = 0.1),
-        submitButton("Submit")
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
+         selectInput("Species", "Species:",
+                     c("Sprat","Sandeel","N.pout","Herring","Dab","Whiting", "Sole","Gurnard","Plaice","Haddock","Cod","Saithe"))
+       )),
+       column(6,
+              plotOutput("distPlot", width = 600, height = 600)
+       )
    )
 )
 
@@ -40,30 +35,36 @@ ui <- fluidPage(
 server <- function(input, output) {
    
   output$distPlot <- renderPlot({
-  relative_effort<-readRDS("relative_effort.RDS")
-  params <- readRDS("params.RDS")
-	# run model
-  #sim2 <- project(params, effort=relative_effort, dt = 0.1, t_save = 1,initial_n = sim@n[200,,],initial_n_pp = 	sim@n_pp[200,])
-  
-	sim2 <- project(params, effort=relative_effort, dt = 0.1, t_save = 1)
-  plotYield(sim2)
-#   # output modelled yields and reshape for plotting
-# 	y <- getYield(sim2)
-# 	y <- reshape2::melt(y)
-# 
-# 	#read in obsy yield values
-# 	obsy <- read.csv("data/obslandings.csv")[,-1]
-# 
-# 	# plot these
-# 	ggplot(y) + geom_line(data=y, aes(x = time, y = log(value), 
-#             colour = sp)) +
-#     geom_point(data=obsy,aes(x = time, y = log(value), 
-#             colour = sp),size=0.1) +
-#     facet_wrap(~sp) +
-#     scale_y_continuous(name = "log Yield [g/year]")  +
-#     scale_colour_manual(values = sim@params@linecolour) +
-#     xlim(1957, 2011)
-
+    
+   
+    #change erepro of selected species
+    params@species_params$erepro[params@species_params$species==input$Species] <- 10^input$erepro
+    #change Rmax of selected species
+    params@species_params$R_max[params@species_params$species==input$Species] <- 10^input$Rmax
+    
+    params <- setReproduction(params)
+    
+    #re-run model
+    simt <- project(params, effort=relative_effort, dt = 0.1, t_save = 1,initial_n = sim@n[200,,],initial_n_pp = sim@n_pp[200,])
+    
+    #re-run model
+    simt <- project(params, effort=relative_effort, dt = 0.1, t_save = 1,initial_n = sim@n[200,,],initial_n_pp = sim@n_pp[200,])
+    
+    # output adjusted modelled yields and reshape for plotting
+    y <- getYield(simt)
+    y <- reshape2::melt(y)
+    
+    
+    p<-ggplot(y) + geom_line(data=filter(y,sp==input$Species), aes(x = time, y = value, 
+                                                             colour = sp)) +
+      geom_point(data=filter(obsy,sp==input$Species),aes(x = time, y = value, 
+                                                   colour = sp),size=0.6) +
+      #facet_wrap(~sp) +
+      scale_y_continuous(name = "Yield [g/year]")  +
+      scale_colour_manual(values = sim@params@linecolour) +
+      xlim(1965, 2011)
+    p
+    
 
    })
 }
